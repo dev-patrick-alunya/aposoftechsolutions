d09c73125a427a034d908cdbe1127664bccce874
dev-patrick-alunya <patrickalunya106@gmail.com>
2025-02-15 18:11:34 +0300

    updates

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,1 +1,14 @@
 document.getElementById('getFullYear').innerHTML = new Date().getFullYear(); // 2024
+
+
+//Start of Tawk.to Script
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/67b0a3a890ec1f1909b0441e/1ik4vujvc';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+        // End of Tawk.to Script
